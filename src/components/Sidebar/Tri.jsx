f29ef99d4e9873183cf451a1db179a88946b0cc8
Thomas <thomas@avecnous.eu>
2021-05-14 13:49:19 +0200

    replace checkbox by radio buttons

--- src/components/Sidebar/Tri.jsx
+++ src/components/Sidebar/Tri.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useContext } from 'react';
 import { AccordionItemPanel } from 'react-accessible-accordion';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 import SlideFilter from './SlideFilter';
 import 'react-accessible-accordion/dist/fancy-example.css';
 import 'font-awesome/css/font-awesome.min.css';
@@ -9,6 +14,11 @@ import './Sidebar.css';
 const Tri = (post) => {
   const [newPost, setNewPost] = useState();
   const [jsonObj, setJsonObj] = useState({});
+  const [value, setValue] = React.useState('female');
+
+  const handleChange = (event) => {
+    setNewPost(event.target.value);
+  };
 
   useEffect(() => {
     sessionStorage.setItem('newPost', newPost);
@@ -24,7 +34,6 @@ const Tri = (post) => {
     setNewPost({ value: e.target.value });
   };
 
-  // remplacer par des radios que des checkbox
   // onClick trier items et mettre à jour
 
   return (
@@ -33,34 +42,40 @@ const Tri = (post) => {
         <div className="dropdown">
           <SlideFilter value={post} />
         </div>
-        <div className="dropdown">
-          <div className="tri">
-            <input type="checkbox" className="checkbox" />
-            <span className="checkmark" />
-            Tri par date de publication
-          </div>
-        </div>
-        <div className="dropdown">
-          <div className="tri">
-            <input type="checkbox" className="checkbox" />
-            <span className="checkmark" />
-            Tri par ordre de contenus
-          </div>
-        </div>
-        <div className="dropdown">
-          <div className="tri">
-            <input type="checkbox" className="checkbox" />
-            <span className="checkmark" />
-            Tri par ordre croissant
-          </div>
-        </div>
-        <div className="dropdown">
-          <div className="tri">
-            <input type="checkbox" className="checkbox" />
-            <span className="checkmark" />
-            Tri par ordre décroissant
-          </div>
-        </div>
+
+        <FormControl component="fieldset">
+          <RadioGroup
+            aria-label="gender"
+            name="gender1"
+            value={newPost}
+            onChange={handleChange}
+          >
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              className="radioButton"
+              label=" Tri par date de publication"
+            />
+            <FormControlLabel
+              value="male"
+              control={<Radio />}
+              className="radioButton"
+              label=" Tri par ordre de contenus"
+            />
+            <FormControlLabel
+              value="other"
+              control={<Radio />}
+              className="radioButton"
+              label="Tri par ordre croissant"
+            />
+            <FormControlLabel
+              value="disabled"
+              control={<Radio />}
+              className="radioButton"
+              label="Tri par ordre décroissant"
+            />
+          </RadioGroup>
+        </FormControl>
       </div>
     </AccordionItemPanel>
   );
