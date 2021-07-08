5106bb7522dfc2c855dcbfc61b60cb7a31945c86
Thomas <thomas@avecnous.eu>
2021-07-08 15:07:44 +0200

    delete nav.jsx

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,36 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ParamsContextProvider from '../Context/ParamsContext';
 import ColorContextProvider from '../Context/ColorContext';
-import Connexion from '../Connexion/Connexion';
 import NavRoute from '../Sidebar/NavRoute';
 import Slider from '../Slider/Slider';
-import Nav from '../Sidebar/Nav';
 
-export default function Routter(props) {
-  const [display] = useState(false);
-  function displayNav() {
-    if (props) {
-      return <Nav />;
-    }
-    // else 'username + token'
-    return <NavRoute />;
-  }
-
+export default function Routter() {
   return (
     <Router>
       <ColorContextProvider>
-        {display ? (
-          displayNav()
-        ) : (
-          <Switch>
-            <Route exact path="/" component={Connexion} />
-            <ParamsContextProvider>
-              <NavRoute />
-              <Route path="/networks/" component={Slider} />
-            </ParamsContextProvider>
-          </Switch>
-        )}
+        <Switch>
+          <ParamsContextProvider>
+            <NavRoute />
+            <Route exact path="/" component={Slider} />
+          </ParamsContextProvider>
+        </Switch>
       </ColorContextProvider>
     </Router>
   );
